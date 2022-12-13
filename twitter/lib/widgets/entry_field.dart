6dcc5fba9d3931e96dbfe8854c0220a438fba5e6
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2022-12-12 20:36:19 -0500

    1. Sign Up/In

---
+++ twitter/lib/widgets/entry_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomEntryField extends StatelessWidget {
+  final String hint;
+  final TextEditingController controller;
+  late final bool isPassword;
+
+  CustomEntryField(
+      {Key? key,
+      required this.hint,
+      required this.controller,
+      required this.isPassword})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: TextField(
+            controller: controller,
+            obscureText: isPassword,
+            decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey.shade200,
+                hintText: hint,
+                border: OutlineInputBorder(
+                    borderSide: const BorderSide(width: 3, color: Colors.blue),
+                    borderRadius: BorderRadius.circular(30))),
+          )),
+    );
+  }
+}
